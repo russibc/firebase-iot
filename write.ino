57b119f711c964182228524455c5aead56d1d13d
russibc <bruna.cr@aluno.ifsc.edu.br>
2021-08-11 00:42:35 -0300

    Writing data to firebase

---
+++ write.ino
@@ -0,0 +1,43 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseESP8266.h>
+
+#define FIREBASE_HOST ""
+#define FIREBASE_AUTH ""
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+FirebaseData firebaseData;
+int id = 0;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  Serial.println("Serial communication started\n\n");
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println();
+  Serial.print("Connected to ");
+  Serial.println(WIFI_SSID);
+  Serial.print("IP Address is : ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+}
+
+void loop() {
+  Firebase.setInt(firebaseData, "/data/test/id", id);
+  Firebase.setString(firebaseData, "/data/test/time","00:35");
+  id++;
+  delay(1000);
+
+}
